492813294ebfe22b2d736b5052725e7973aea221
DarsiSreelekha <89741061+DarsiSreelekha@users.noreply.github.com>
2021-08-30 00:07:35 +0530

    Add files via upload

---
+++ src/head.spec.js
@@ -0,0 +1,10 @@
+const head = require('./head');
+describe('head',() =>{
+    it('Head of [1,2,3] is 1',() =>{
+        expect(head([1,2,3])).toEqual(1);
+    });
+    it('Head of [] is null or undefined',() =>{
+        expect(head([])).toEqual(null || undefined);
+    });
+}
+)
